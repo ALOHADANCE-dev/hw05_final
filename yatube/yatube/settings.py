850c0e5abf6d7f8f393e1fc1f63137b8d4c62343
Данила Удовиченко <danyarusskiy17@mail.ru>
2023-05-29 13:29:12 +0400

    Финальный фиксы после ревью, проект сдан!

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -11,6 +11,8 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '[::1]',
     'testserver',
+    'www.Yggdora.pythonanywhere.com',
+    'Yggdora.pythonanywhere.com',
 ]
 
 
@@ -28,6 +30,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'about.apps.AboutConfig',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -38,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
@@ -117,3 +121,7 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     }
 }
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
